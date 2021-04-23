408dd48344d0cfa9c6bc21c399ed201ac424abc6
Samuel-Trujillo <samuel.trujillo093@gmail.com>
2021-04-22 19:00:02 -0700

    commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//odds
+for(var i=0;i<21;i++){
+    if(i%2!=0) console.log(i)
+}
+
+// sequence
+for(var i=4;i>-4;i-=1.5){
+    console.log(i)
+}
+
+//sigma
+var sum=0
+for(i=0; i<101;i++){
+    sum=sum+i
+}
+console.log(sum)
+
+//Facorial
+var n=0
+var x=0
+while(1e8 != n*x){
+    n++;
+    x++;
+}
+console.log(n)
